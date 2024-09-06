7b224e69f38b7992e2c910fb9d0111de8ba442e0
sebastianG <645310170@qq.com>
2024-09-06 12:33:50 +0800

    update svrCfg and zonetime

--- Server/src/game_server/game_server.cpp
+++ Server/src/game_server/game_server.cpp
@@ -2,6 +2,39 @@
 #include "game_server.h"
 #include <iostream>
 
+CGameServer::CGameServer()
+{
+
+}
+
+
+CGameServer::~CGameServer()
+{
+
+}
+
+BOOL CGameServer::Init(const char* szConfig)
+{
+
+}
+
+
+BOOL CGameServer::MainLoop(const char* szConfig)
+{
+
+}
+
+void CGameServer::CreateReloadThread(const char* szConfig)
+{
+
+}
+
+BOOL CGameServer::CanExit()
+{
+
+}
+
+
 int main()
 {
 
--- Server/src/game_server/game_server.h
+++ Server/src/game_server/game_server.h
@@ -1,6 +1,24 @@
 #ifndef _GAME_SERVER_H__
 #define _GAME_SERVER_H__
 
+#include "fram_server/fram_server.h"
 
+class CGameServer :public CFramServer
+{
+public:
+    CGameServer();
+    virtual ~CGameServer();
+
+public:
+
+    BOOL Init(const char* szConfig);
+
+    BOOL MainLoop(const char* szConfig);
+
+    void CreateReloadThread(const char* szConfig);
+
+    virtual BOOL canExit() override;
+
+};
 
 #endif //_GAME_SERVER_H__
